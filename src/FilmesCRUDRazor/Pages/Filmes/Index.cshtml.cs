d7956691cee854a3085941a9c7eb7486921cac98
AlisonMarques <alison-marques@outlook.com.br>
2021-04-03 12:09:20 -0300

    Adicionado o filtro por genero e nome.

--- src/FilmesCRUDRazor/Pages/Filmes/Index.cshtml.cs
+++ src/FilmesCRUDRazor/Pages/Filmes/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FilmesCRUDRazor.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FilmesCRUDRazor.Pages.Filmes
 {
@@ -19,10 +20,37 @@ namespace FilmesCRUDRazor.Pages.Filmes
         }
 
         public IList<Filme> Filme { get;set; }
+        public SelectList Generos;
+        public string FilmePorGenero { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            Filme = await _context.Filme.ToListAsync();
+        public async Task OnGetAsync(string buscaPorGeneroNome, string filmePorGenero)
+        {   
+            #region Inicio lógica do input
+            /* select genero from Filme where filme.genero == genero*/
+            IQueryable<string> queryGenero = from f in _context.Filme orderby f.Genero select f.Genero;
+
+            /* select * from filme */
+            var filmes = from f in _context.Filme select f;
+
+            // Se não está vázio ou nulo, pegue o array filmes e faz um arrow function
+            // pegando apenas o titulo passado em buscaPorGeneroNome
+            if(!String.IsNullOrEmpty(buscaPorGeneroNome))
+            {
+                filmes = filmes.Where(b => b.Titulo.Contains(buscaPorGeneroNome.Trim()));
+            }
+            #endregion Fim lógica do input
+
+            #region Lógica do select
+            if(!String.IsNullOrEmpty(filmePorGenero))
+            {
+                filmes = filmes.Where(b => b.Genero == filmePorGenero);
+            }
+            #endregion
+
+            Generos = new SelectList(await queryGenero.Distinct().ToListAsync());
+           
+            Filme = await filmes.ToListAsync();
+            
         }
     }
 }
